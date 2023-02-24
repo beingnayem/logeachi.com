6e41ae5345a0eed327c7d5a82952efa0b4de4621
Nayem Sarwar <nayemsarwar12345@gmail.com>
2023-02-24 16:48:14 +0600

    all done

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 # Create your views here.
 
-def index(request):
-    context = {'products': Product.objects.all()}
+def index(request,):
+    context = {'products': Product.objects.all(), 'categories': Category.objects.all()}
     return render(request, 'home.html' , context)
