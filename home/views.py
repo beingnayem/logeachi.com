47e8eb45388ff5fc6f87c4e9d4f51fcb0007994a
Naim <mdnaim01303202218@gmail.com>
2023-02-27 00:30:06 +0600

    model

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from products.models import Product, Category
-from home.models import AlertNews
+from home.models import AlertNews , BannerSlider
+
 # Create your views here.
 
 def index(request,):
 
-    context = {'products': Product.objects.all(), 'categories': Category.objects.all(), 'Alert_news': AlertNews.objects.all()}
+    context = {'products': Product.objects.all(), 'categories': Category.objects.all(), 'Alert_news': AlertNews.objects.all(), 'sliders':BannerSlider.objects.all()}
     
     return render(request, 'home/index.html' , context)
