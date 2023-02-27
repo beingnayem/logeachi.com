773683d49ddf4cb63008c034a8f55a2142da3cde
Nayem Sarwar <nayemsarwar12345@gmail.com>
2023-02-27 22:36:12 +0600

    ui set

--- home/views.py
+++ home/views.py
@@ -4,12 +4,25 @@ from home.models import AlertNews , BannerSlider
 
 # Create your views here.
 
-def index(request,):
-    female_collection = Product.objects.filter(tergeted_buyer=       'Only Female')
-    male_collection = Product.objects.filter(tergeted_buyer = 'Only Male')
-    kids_collection = Product.objects.filter(tergeted_buyer = 'Only Kids')
-    both_collection = Product.objects.filter(tergeted_buyer = 'Male & Female')
+def index(request):
     
-    context = {'products': Product.objects.all(), 'categories': Category.objects.all(), 'Alert_news': AlertNews.objects.all(), 'sliders':BannerSlider.objects.all(), 'female_collc': female_collection, 'male_collc': male_collection, 'kids_collc': kids_collection, 'both_collc': both_collection }
+    # female_collection = Product.objects.filter(tergeted_buyer=       'Only Female')
+    # male_collection = Product.objects.filter(tergeted_buyer = 'Only Male')
+    # kids_collection = Product.objects.filter(tergeted_buyer = 'Only Kids')
+    # both_collection = Product.objects.filter(tergeted_buyer = 'Male & Female')
+
+    categoryID = request.GET.get('category')
+
+    if categoryID:
+        product = Product.objects.filter(category=categoryID)
+
+    else:
+        product = Product.objects.all()
+
+    # context = {'cat_products':cat_products}
+    
+    # return render(request, 'products/category_products.html', context)
+    
+    context = {'products': product, 'categories': Category.objects.all(), 'Alert_news': AlertNews.objects.all(), 'sliders':BannerSlider.objects.all(), }
     
     return render(request, 'home/index.html' , context)
