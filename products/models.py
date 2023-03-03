3f8a2d07961fef47d47aa7ffecf66915c67c3e84
Nayem Sarwar <nayemsarwar12345@gmail.com>
2023-03-03 15:34:19 +0600

    all done

--- products/models.py
+++ products/models.py
@@ -22,22 +22,15 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    
-    type=[
-        ('Male & Female', 'Uni-sex'),
-        ('Only Male',  'Male'),
-        ('Only Female', 'Female'),
-        ('Only Kids', 'Kids')
-    ]
 
     product_name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='product')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    tergeted_buyer = models.CharField(max_length=50, choices=type, default='Uni-sex')
     added_date = models.DateTimeField(auto_now_add=True, null=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
     price = models.IntegerField()
     product_description = models.TextField()
+    quantity = models.IntegerField(null=True)
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.product_name)
@@ -45,4 +38,3 @@ class Product(models.Model):
     
     def __str__(self) -> str:
         return self.product_name
-
