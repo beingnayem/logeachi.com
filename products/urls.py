c5408ab350b64e5a6301c90810342a6f5cf6bc8d
Nayem Sarwar <nayemsarwar12345@gmail.com>
2023-03-05 04:54:23 +0600

    category wise set

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from products import views
 
 urlpatterns = [     
     path('<slug>/',  views.get_product, name='get_product'),
+    path('category/<pk>', views.category_products, name='category-products')
 ]
