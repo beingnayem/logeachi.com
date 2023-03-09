1d1592f53d3ff1f34aa2517490a8a6d472a7735d
Nayem Sarwar <nayemsarwar12345@gmail.com>
2023-03-09 16:41:41 +0600

    address updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('reset-pass/<uidb64>/<token>/', views.SetNewPasswordView.as_view(), name='reset-pass'),
     path('show-profile/', views.showProfile, name='show-profile'),
     path('show-address/', views.showAddress, name='show-address'),
+    path('add-address/', views.addAddress, name='add-address'),
+    path('edit-address/<int:address_id>/', views.editAddress, name='edit-address'),
+    path('delete-address/<int:address_id>/', views.deleteAddress, name='delete-address'),
 ]
