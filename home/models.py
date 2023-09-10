a5fe4eaa0d1b88bda7a496cc51902bc73e91d4f0
Antar Chandra Nath <antarnathbd@gmail.com>
2023-09-10 23:23:08 +0600

    toster,logo issue fixed

--- home/models.py
+++ home/models.py
@@ -8,10 +8,12 @@ class Banner(models.Model):
     ('slider','Slider'),
     ('newslatter', 'Newslatter'), 
     ('promotion', 'Promotion'), 
+    ('toster', 'Toster'),
     )
     banner_name=models.CharField(max_length=200)
     banner_type=models.CharField(max_length=200, choices=TypeChoice)
     banner_image=models.ImageField(upload_to='banner')
+    
 
     def __str__(self) -> str:
         return self.banner_name
