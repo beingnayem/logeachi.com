6c76cafd686838d3f6aedb8b7553410425289dda
Being-Nayem <nayemsarwar12345@gmail.com>
2023-09-13 22:21:56 +0600

    static delete

--- logeachi/static/assets/js/admin_panel.js
+++
@@ -1,9 +0,0 @@
-let adminPanelsidebar = document.querySelector(".adminPanelsidebar");
-let AdminPanelsidebarBtn = document.querySelector(".AdminPanelsidebarBtn");
-AdminPanelsidebarBtn.onclick = function() {
-  adminPanelsidebar.classList.toggle("active");
-  if(adminPanelsidebar.classList.contains("active")){
-    AdminPanelsidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
-}else
-AdminPanelsidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-}
