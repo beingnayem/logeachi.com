c4a03f69eb12b6484e47a1004270ef90d147f0fd
Being-Nayem <nayemsarwar12345@gmail.com>
2023-09-17 02:05:39 +0600

    fix message and edit product

--- static/assets/js/admin_panel.js
+++ static/assets/js/admin_panel.js
@@ -7,3 +7,25 @@ AdminPanelsidebarBtn.onclick = function() {
 }else
 AdminPanelsidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
 }
+
+
+
+function goBack() {
+  window.history.back();
+}
+
+// JavaScript function for cancel action (you can customize this)
+function cancel() {
+  alert("Cancelled!");
+}
+
+
+
+function toggleDetails() {
+  var details = document.querySelector('.individual-product-details');
+  if (details.style.display === 'none' || details.style.display === '') {
+      details.style.display = 'block';
+  } else {
+      details.style.display = 'none';
+  }
+}
