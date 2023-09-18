e2a8f4ab08eb83159ebde32ef706c416fd4bac71
Being-Nayem <nayemsarwar12345@gmail.com>
2023-09-18 18:26:18 +0600

    added query sending system and query reply

--- home/models.py
+++ home/models.py
@@ -18,5 +18,32 @@ class Banner(models.Model):
     def __str__(self) -> str:
         return self.banner_name
 
-class Subscribers(models.Model):
+class Newsletter(models.Model):
+    GenderChoiche=(
+        ('male','Male'),
+        ('female','Female'),
+    )
     email= models.EmailField(max_length=155)
+    gender = models.CharField(max_length=10, choices=GenderChoiche, blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.email
+
+
+class Queries(models.Model):
+    QueryChoiche=(
+        ('recived','Recived'),
+        ('replyed','Replyed'),
+    )
+    name = models.CharField(max_length=155)
+    email= models.EmailField(max_length=155)
+    subject= models.CharField(max_length=255)
+    query_message= models.TextField(max_length=1000)
+    query_date = models.DateTimeField(auto_now_add=True)
+    query_status = models.CharField(max_length=10, choices=QueryChoiche, default='recived')
+    reply_date = models.DateTimeField(auto_now=True)
+    query_reply= models.TextField(max_length=1000, null=True, blank=True, default=None)
+    
+    def __str__(self) -> str:
+        return self.name
+
