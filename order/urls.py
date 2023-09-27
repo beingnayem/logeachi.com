b0f96c83ca834b9d36d4b9812c8fe2a99bda7486
Antar Chandra Nath <antarnathbd@gmail.com>
2023-09-28 03:06:23 +0600

    working on sslcommerz payment system

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('palce-order/', views.place_order, name='place_order'),
+    path('success-view/', views.success_view, name='success_view'),
 ]
