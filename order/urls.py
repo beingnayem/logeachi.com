f79f76514498422d35829f79e0df293620baec29
Antar Chandra Nath <antarnathbd@gmail.com>
2023-09-30 22:49:16 +0600

    worked on order system

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('palce-order/', views.place_order, name='place_order'),
     path('success-view/', views.success_view, name='success_view'),
+    path('success-view-case-on-delivery/<int:order_id>/<grand_total>/', views.success_view_case_on_delivery, name='success_view_case_on_delivery'),
 ]
