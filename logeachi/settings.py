c2fef58a2b4cf141a97c204614aa5ee829827bd1
Being-Nayem <nayemsarwar12345@gmail.com>
2023-10-03 19:24:32 +0600

    settings update 6

--- logeachi/settings.py
+++ logeachi/settings.py
@@ -15,7 +15,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY'),
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
